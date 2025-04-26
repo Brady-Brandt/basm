d5230a20e29f5e5f640a136d9d5872e1461dbf2e
Brady-Brandt <bradybrandt3@gmail.com>
2025-04-26 09:13:26 -0500

    Added support for tables that go across several pages

--- x86/extractor.py
+++ x86/extractor.py
@@ -7,10 +7,44 @@ instructions = {}
 
 unsupported = {}
 
+
+letters = []
+
 doc = pymupdf.open("intel.pdf") # open a document
-out = open("output.txt", "w") # create a text output
+
+
+out = open("instructions.dat", "w") # create a text output
+
+def write_opcode_table(file, op_table):
+    in_op_en = False
+
+    if "/" in op_table[0][0]:
+        in_op_en = True
+
+    for i in range(1,len(op_table)):
+        row = op_table[i]
+
+        if row[1] == None:
+            break
+
+        if in_op_en:
+            nl = row[0].find('\n')
+            padding = 25 - len(row[0][:nl])
+            row[0] = row[0].replace("\n", padding * " " + "|")
+            file.write(row[0] + row[1] + "\n")
+        else:
+            row[1] = row[1].replace("*", "")
+            row[1] = row[1].replace("\n", "")
+            padding= 25 - len(row[0])
+            file.write(row[0] + padding * " " + "|" + row[1] + "\n")
+
+
+
 #128,638
-for page in doc.pages(128,638): # iterate the document pages
+#638,2266
+
+prev_page = None
+for page in doc.pages(128, 2266): # iterate the document pages
     words = page.get_text("words") # get plain text (is in UTF-8)
 
     inst = words[0][4]
@@ -20,7 +54,11 @@ for page in doc.pages(128,638): # iterate the document pages
         continue
 
     if inst not in instructions and inst not in unsupported:
-        print(page)
+        if inst[0] not in letters:
+            print(inst[0])
+            letters.append(inst[0])
+
+
         tables = page.find_tables()
 
         if tables.tables == []:
@@ -34,43 +72,27 @@ for page in doc.pages(128,638): # iterate the document pages
         if 'CPUID' in op_table[0][3]:
             # not gonna support these instructions yet
             unsupported[inst] = op_table
+            prev_page = None
             continue 
+
+
+        prev_page = inst
 
         out.write(inst + "\n")
 
-        in_op_en = False
-
-        if "/" in op_table[0][0]:
-            in_op_en = True
-
-
-        for i in range(1,len(op_table)):
-            row = op_table[i]
-
-            if row[1] == None:
-                break
-
-            if in_op_en:
-                nl = row[0].find('\n')
-                padding = 25 - len(row[0][:nl])
-                row[0] = row[0].replace("\n", padding * " " + "|")
-                out.write(row[0] + row[1] + "\n")
-            else:
-                row[1] = row[1].replace("*", "")
-                row[1] = row[1].replace("\n", "")
-                padding= 25 - len(row[0])
-                out.write(row[0] + padding * " " + "|" + row[1] + "\n")
-
-
-            """
-            flag = "null"
-
-            if exten:
-                flag = row[3] 
-
-            f_pad = 15 - len(row[1])
-            """
-
-
+        write_opcode_table(out, op_table)
         instructions[inst] = op_table
+    elif prev_page != None:
+        # some tables go to the next page
+        tables = page.find_tables()
+        if tables.tables == []:
+           continue 
+        op_table = tables.tables[0].extract()
+
+        # check if its an opcode table
+        if "Opcode" in op_table[0][0]:
+            write_opcode_table(out, op_table) 
+
+    else:
+        prev_page = None
 out.close()
