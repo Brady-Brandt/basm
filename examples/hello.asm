66939f1d395a6ab8b4320eb45ee403d72196101e
Brady-Brandt <bradybrandt3@gmail.com>
2026-07-08 14:00:21 -0500

    updated to include both linux and mac

--- examples/hello.asm
+++ examples/hello.asm
@@ -1,23 +1,37 @@
-section .data 
-    hello: db "Hello, World!" ; Strings are automatically null terminated
-    goodbye: db "Adios"
-
-section .text
-
+#if __ELF__
 global _start
-
 extern puts
 extern exit
-
 _start:
-    #ifdef __LINUX__
-        lea rdi, [hello]
-        call puts
-        lea rdi, [goodbye]
-        call puts
+    lea rdi, [hello]
+    call puts
 
-        xor rdi, rdi
-        call exit
-    #endif
+    lea rdi, [goodbye]
+    call puts
 
+    xor edi, edi
     call exit
+#elif __MACHO__
+global _main
+extern _puts
+extern _exit
+_main:
+    lea rdi, [hello]
+    call _puts
+
+    lea rdi, [goodbye]
+    call _puts
+
+    xor edi, edi
+    call _exit
+#elif __WIN__
+; windows implementation not supported yet
+global main
+extern exit
+main:
+    call exit
+#endif
+
+section .data
+    hello: db "你好"
+    goodbye: db "Adios"
